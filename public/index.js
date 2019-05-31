7c0f79c1db93e3910f41b4e1857e3a73c4512935
AndreaReyesSerna <andrea.serna.quin@gmail.com>
2019-05-31 14:41:22 -0500

    preparar para subir a web

--- public/index.js
+++ public/index.js
@@ -79,7 +79,7 @@ window.addEventListener('load', function () {
     TweenLite.from(box, 1, { x: 500, opacity: 0, ease: Sine.easeIn});
 
     var cards = document.querySelector('.features__card-container');
-    TweenLite.from(cards, 2, { autoAlpha: 0 , delay: 6});
+    TweenLite.from(cards, 2, { autoAlpha: 0 , delay: 3});
     
     var titGaleria= document.querySelector('.gallery__title__h1');
     TweenLite.from(titGaleria, 1, {x: -300, ease: Sine.easeIn, delay: 6 });
